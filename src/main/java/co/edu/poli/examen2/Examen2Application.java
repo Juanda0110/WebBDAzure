4fa50498a0443901970bf43440a71b367f13c4e0
Juanda <pinedaovallejuandavid@gmail.com>
2025-05-27 13:08:50 -0500

    Actualiza workflow de GitHub Actions

--- src/main/java/co/edu/poli/examen2/Examen2Application.java
+++ src/main/java/co/edu/poli/examen2/Examen2Application.java
@@ -2,13 +2,13 @@ package co.edu.poli.examen2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories("co.edu.poli.examen2.repositories")
 public class Examen2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Examen2Application.class, args);
 	}
-
-
 }
