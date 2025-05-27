2031229bff4b762f19064c8c6da0aa38126623d3
Juanda <pinedaovallejuandavid@gmail.com>
2025-05-27 12:24:05 -0500

    Actualiza workflow de GitHub Actions

---
+++ src/main/java/co/edu/poli/examen2/controller/HomeController.java
@@ -0,0 +1,13 @@
+package co.edu.poli.examen2.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String index() {
+        return "¡Aplicación desplegada exitosamente en Azure!";
+    }
+}
